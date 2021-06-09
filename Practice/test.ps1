4799f06b97cef652ddc6c310c83930c87410cc73
Venkatesh <57030342+krishvenkybe@users.noreply.github.com>
2021-06-09 10:57:59 +0530

    Changes

--- Practice/test.ps1
+++ Practice/test.ps1
@@ -1,5 +1,5 @@
 #region (Ping test for list of machines)
-$systems =  Import-Csv -Path "C:\Users\vradhakrishnan\Downloads\Asset_Report_Nov2019.csv" | `
+$systems =  Import-Csv -Path "C:\Users\v********\Downloads\Asset_Report_Nov2019.csv" | `
             Select-Object -ExpandProperty "Machine Name"
 # $systems | Get-Member
 # #Ping Test.
@@ -7,14 +7,14 @@ foreach($pcs in $systems)
     {
         if(Test-Connection $pcs -Count 1 -Quiet)
         {
-            Write-Output "$pcs, Success" | Out-File -FilePath "C:\Users\vradhakrishnan\Desktop\test.txt" -Append
+            Write-Output "$pcs, Success" | Out-File -FilePath "C:\Users\v**********\Desktop\test.txt" -Append
         }
         else
         {
-            Write-Output "$pcs, Failed" | Out-File -FilePath "C:\Users\vradhakrishnan\Desktop\test.txt" -Append
+            Write-Output "$pcs, Failed" | Out-File -FilePath "C:\Users\v************\Desktop\test.txt" -Append
         }
     }
-$1 = Get-Content -Path "C:\Users\vradhakrishnan\Desktop\test.txt"
-$2 = $1 | Export-Csv -Delimiter ',' -Path "C:\Users\vradhakrishnan\Desktop\test1.csv"
+$1 = Get-Content -Path "C:\Users\v**********\Desktop\test.txt"
+$2 = $1 | Export-Csv -Delimiter ',' -Path "C:\Users\v*********\Desktop\test1.csv"
 
 #endregion
